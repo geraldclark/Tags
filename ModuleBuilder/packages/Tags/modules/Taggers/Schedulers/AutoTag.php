2ceeaad572189cd04c520673ab8a5f3ec4141860
Jerry Clark <geraldbclark@gmail.com>
2012-12-06 17:50:13 -0500

    Update ModuleBuilder/packages/Tags/modules/Taggers/Schedulers/AutoTag.php

--- ModuleBuilder/packages/Tags/modules/Taggers/Schedulers/AutoTag.php
+++ ModuleBuilder/packages/Tags/modules/Taggers/Schedulers/AutoTag.php
@@ -2,9 +2,6 @@
 
     if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
-    require_once('modules/tag_Tags/TagSettings.php');
-    $settings = new TagSettings();
-
     if (!isset($limit))
     {
         $limit = 200;
